a0190811f775820126a833f3874539a28e845a26
Alexyong12 <houkionyong@50gmail.com>
2026-04-30 16:10:48 +0700

    Updates

---
+++ _next/static/UG7pmpCpOlDbFjZgE4ewt/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ecommerce1-with-next//_next/:path+",
+        "destination": "/ecommerce1-with-next/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
