ad63aab5eceed5047a8ddec05bc28eecabaa8d66
Alexyong12 <houkionyong@50gmail.com>
2026-04-30 17:03:41 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  //output: "export",
   reactCompiler: true,
-  cacheComponents:true,
-  basePath: "/ecommerces1",
-  assetPrefix: "/ecommerces1/",
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Slider from "./components/slide";
 export default function Home() {
   return(
     <article className="flex flex-col">
-      <Slider />
+      <Slider /> 
+     
     </article>
   );
 }
